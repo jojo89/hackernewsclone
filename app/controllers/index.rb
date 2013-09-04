2b39e9863a352e57b155b01cf4adc4d48df73089
Corey Speisman <Cspeisman@gmail.com>
2013-09-04 11:05:10 -0500

    user stuff

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,6 +1,19 @@
 get '/' do
   # Look in app/views/index.erb
   @posts = Post.all
-
   erb :index
 end
+
+get '/home' do 
+	if session[:user_id]
+		@posts = Post.all
+	  erb :home
+	end
+end
+
+get '/logout' do 
+	p session
+	session.clear
+	p session
+end
+
--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,6 +1,18 @@
-get '/' do
-  # Look in app/views/index.erb
+# GETS ==================================
 
-
+get '/login' do
   erb :login
+end
+
+
+# POSTS =================================
+
+post '/login' do 
+	user = User.find_by_email(params[:email])
+	if user && user.password == params[:password]
+		session[:user_id] = user.id
+		redirect '/home'
+	else
+		redirect '/login'
+	end
 end
